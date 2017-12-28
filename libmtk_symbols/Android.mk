310728685f229dbfb74f6ffdd956e8a3f8af2439
Raj Kumar <33460256+Attached2Android@users.noreply.github.com>
2017-12-29 01:15:55 +0530

    Update Android.mk

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -1,24 +1,18 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-             AudioStubs.cpp \
-             Bionic.cpp \
-             EGL.cpp \
-             GraphicBufferMapper.cpp \
-             Icu53.cpp \
-             Icu55.cpp \
-             MediaBufferGroup.cpp \
-             MediaSource.cpp \
-             Player.cpp \
-             RIL.cpp \
-             SSL.cpp \
-             WVM.cpp \
-             Xlog.cpp
+    crypto_malloc.c \
+    mtk_wvm.cpp \
+    mtk_ui.cpp \
+    mtk_ril.cpp \
+    icu51.c \
+    icu53.c \
+    icu55.c \
+    bionic.cpp
 
-LOCAL_SHARED_LIBRARIES := libbinder libcutils libutils liblog libicuuc libui libcrypto libc libssl
+LOCAL_SHARED_LIBRARIES := libbinder liblog libicuuc libui libcrypto
 LOCAL_MODULE := libmtk_symbols
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
