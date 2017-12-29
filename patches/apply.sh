22860a4ec5933e056e55d83c1b7e261604b3dc35
Raj Kumar <33460256+Attached2Android@users.noreply.github.com>
2017-12-29 13:52:02 +0530

    Update apply.sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+cd ../../../..
+cd  frameworks/av
+patch -p1 < ../../device/LAVA/IRISX8/patches/frameworks_av-1.patch
 patch -p1 < ../../device/LAVA/IRISX8/patches/frameworks_av.patch
 
 cd ../..
 cd apps/Settings
 patch -p1 -b < ../../../device/LAVA/IRISX8/patches/dev_info.patch
+
+cd ../..
 
 echo Successfuly patched! Go ahead and start building!
